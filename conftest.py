508c3507676d1d0202791add8a9f241ccc204b3a
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-30 10:19:35 +0300

    Обновил настройки браузера Chrome

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
+import os
+
 from pytest import fixture
 from selenium import webdriver
 
 
-@fixture()
+@fixture(scope='session')
 def initialized_browser():
-    browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("prefs", {
+        "download.default_directory": f"{os.getcwd()}",
+        "download.prompt_for_download": False,
+        "safebrowsing.enabled": True,
+        "safebrowsing.disable_download_protection": True
+    })
+
+    browser = webdriver.Chrome(options=chrome_options)
     browser.implicitly_wait(10)
     yield browser
     browser.close()
