7056258c7e7cf6d44127da21adc725125815c024
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-30 10:21:30 +0300

    Написал тесты для второго сценария

---
+++ tests/test_sbis_contacts_page.py
@@ -0,0 +1,63 @@
+from pytest import mark
+from selenium.webdriver.support import expected_conditions
+
+from pages.sbis_contacts_page import SbisContactsPage, region_chooser_text_selector, YAR_OBL_LABEL, KAMCHATKA_KRAI_LABEL
+
+
+def partners_verify(page: SbisContactsPage):
+    assert page.partners_list is not None
+    assert len(page.partners) > 0
+
+
+def region_chooser_real_region_verify(page: SbisContactsPage):
+    region_chooser_text = page.region_chooser_text
+    assert region_chooser_text == YAR_OBL_LABEL
+
+
+def region_chooser_is_exists(page: SbisContactsPage):
+    # находим кнопку для смены региона
+    region_chooser = page.region_chooser
+    # ждём, когда она станет кликабельной
+    page.wait_until(expected_conditions.element_to_be_clickable(region_chooser))
+    # нажимаем на неё
+    region_chooser.click()
+
+    # находим на странице модальное окно
+    region_chooser_dialog = page.region_chooser_dialog
+    # проверяем его наличие
+    assert region_chooser_dialog is not None
+
+
+def region_choose_dialog_verify(page: SbisContactsPage):
+    # ждём появляения поисковой строки в модальном окне, что будет означать, что окно полностью загрузилось
+    page.wait_until(expected_conditions.visibility_of(page.region_chooser_dialog_searchbar))
+
+    # находим на странице вариант "Камчатский край"
+    kamchatka_krai_dialog_option = page.kamchatka_krai_dialog_option
+    # ждём, пока он не станет кликабельным
+    page.wait_until(expected_conditions.element_to_be_clickable(kamchatka_krai_dialog_option))
+    # нажимаем на него
+    kamchatka_krai_dialog_option.click()
+
+    # ждём пока текст на кнопке смены региона не поменяется на другой (любой)
+    page.wait_until_not(
+        expected_conditions.text_to_be_present_in_element(region_chooser_text_selector, YAR_OBL_LABEL)
+    )
+
+    # получаем новый текст
+    update_region_chooser_text = page.region_chooser_text
+
+    assert KAMCHATKA_KRAI_LABEL in update_region_chooser_text
+    assert KAMCHATKA_KRAI_LABEL in page.browser.title
+
+
+@mark.scenario_2
+def test_sbis_contacts_page(initialized_browser):
+    sbis_contacts_page = SbisContactsPage(initialized_browser)
+    sbis_contacts_page.open()
+
+    region_chooser_real_region_verify(sbis_contacts_page)
+    partners_verify(sbis_contacts_page)
+
+    region_chooser_is_exists(sbis_contacts_page)
+    region_choose_dialog_verify(sbis_contacts_page)
